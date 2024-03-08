aac4ceed44c077b566b3c4a756fcd0f543288a49
Tim Liu <timho1985@gmail.com>
2024-03-08 12:23:30 -0800

    Review comments: fix comment error log in console

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,31 +1,63 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Comment.scss';
 import {getDateStr} from '../../utils/Util.js'
 import head from '../../assets/images/Mohan-muruge.jpg';
 
 // Display comment form and comment list for selected video
 function Comments({selectedVideoDetail}) {
+    const commentMobileRef = useRef();
+    const commentTabletRef = useRef();
     const isEmpty = Object.keys(selectedVideoDetail).length === 0;
 
     if (isEmpty) {
         return <div>No details available.</div>;
     }
 
+    function handleTabletSubmit(event) {
+        event.preventDefault();
+
+        // Access input values from refs
+        const comment = commentTabletRef.current.value;
+        console.log("handleTabletSubmit Comment:", comment);
+
+        commentTabletRef.current.value = '';
+    }
+    function handleMobileSubmit(event) {
+        event.preventDefault();
+
+        // Access input values from refs
+        const comment = commentMobileRef.current.value;
+        console.log("handleMobileSubmit Comment:", comment);
+
+        commentMobileRef.current.value = '';
+    }
     return (
         <div>
             <div className='form'>
                 <img className="form__img" src={head} alt="Mohan Muruge"/>
-                <form className='form__container'>
-                    <label className='form__label' htmlFor="comment">JOIN THE CONVERSATION</label>
+                <form className='form__container form__tablet-hide' onSubmit={handleMobileSubmit}>
+                    <label className='form__label' htmlFor="commentMobile">JOIN THE CONVERSATION</label>
                     <textarea
-                        className="form__input tablet-hiden"
+                        className="form__input"
                         type="text"
-                        id="comment"
-                        name="userComment"
+                        id="commentMobile"
+                        name="commentMobile"
                         placeholder="Add a new commment"
                         required
+                        ref={commentMobileRef}
                     ></textarea>
-                    <input className="form__input mobile-hiden" type="text" id="comment" name="userComment" placeholder="Add a new commment" required/>
+                    <button className='form__button' type="submit">COMMENT</button>
+                </form>
+                <form className='form__container form__mobile-hide' onSubmit={handleTabletSubmit}>
+                    <label className='form__label' htmlFor="commentTablet">JOIN THE CONVERSATION</label>
+                    <input
+                        className="form__input"
+                        type="text"
+                        id="commentTablet"
+                        name="commentTablet"
+                        placeholder="Add a new commment"
+                        required
+                        ref={commentTabletRef}/>
                     <button className='form__button' type="submit">COMMENT</button>
                 </form>
             </div>
